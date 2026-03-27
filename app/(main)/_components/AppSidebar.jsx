000e057793e932e2bc7c9b511f56b5d0fc8be416
Tript-kak <kakraynetript@gmail.com>
2026-03-28 02:01:16 +0530

    added sidebar components

--- app/(main)/_components/AppSidebar.jsx
+++ app/(main)/_components/AppSidebar.jsx
@@ -1,18 +1,57 @@
+"use client"
+import Image from "next/image"
+import { Plus } from "lucide-react"
+import { Button } from "@/components/ui/button"
+import {SideBarOptions} from "@/services/Constants"
+import Link from "next/link"
+import { usePathname } from "next/navigation"
 import {
   Sidebar,
   SidebarContent,
   SidebarFooter,
   SidebarGroup,
   SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+ 
 } from "@/components/ui/sidebar"
 
+
 export function AppSidebar() {
+
+  const path = usePathname();
   return (
     <Sidebar> 
-      <SidebarHeader />
+      <SidebarHeader className= 'flex items-center mt-5' >
+        <Image src = {'/logo.png'} alt = "logo" width = {200} 
+          height = {100}  
+          className="w-[200px] h-[150px]"
+        />
+        <Button className={'w-full mt-5'}> <Plus/> Create New Interview</Button>
+        </SidebarHeader>
       <SidebarContent>
-        <SidebarGroup />
-        <SidebarGroup />
+        <SidebarGroup >
+       
+          <SidebarMenu>
+            
+              {SideBarOptions.map((option,index)=>(
+                <SidebarMenuItem key = {index} className={'p-1'}>
+                <SidebarMenuButton asChild className={'p-5'}>
+                  <Link href = {option.path}>
+                  <option.icon/>
+                  <span className={`text-[16px] ${path === option.path ? "text-primary" : ""}`}>
+                  {option.name}
+                  </span>
+                  </Link>
+                </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+          </SidebarMenu>
+          
+
+          
+        </SidebarGroup >
       </SidebarContent>
       <SidebarFooter />
     </Sidebar>
---
+++ services/Constants.jsx
@@ -0,0 +1,34 @@
+import { Calendar, LayoutDashboard, Settings, WalletCards } from "lucide-react";
+import { List } from "lucide-react"
+export const SideBarOptions=[
+    {
+    name:'Dashboard',
+    icon: LayoutDashboard,
+    path: '/dashboard'
+    },
+
+     {
+    name:'Scheduled Interview',
+    icon: Calendar,
+    path: '/scheduled-interview'
+    },
+
+     {
+    name:'All Interview',
+    icon: List,
+    path: '/all-interview'
+    },
+
+     {
+    name:'Billing',
+    icon: WalletCards,
+    path: '/billing'
+    },
+
+     {
+    name:'Setting',
+    icon: Settings,
+    path: '/setting'
+    }
+
+]
